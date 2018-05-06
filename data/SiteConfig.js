637e78f9b9b4d8a813e1fef14ee077a90b4202c0
Gavin Mogan <git@gavinmogan.com>
2018-05-06 11:37:17 -0700

    switch to real content

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  blogPostDir: "sample-posts", // The name of directory that contains your posts.
+  blogPostDir: "posts", // The name of directory that contains your posts.
   siteTitle: 'The Nameless Site', // Site title.
   siteTitleAlt: 'The Nameless Site', // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
