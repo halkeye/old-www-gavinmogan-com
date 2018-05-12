d5f9a3d28dc5490318279eba5d66f8428550efc1
Gavin Mogan <git@gavinmogan.com>
2018-05-11 20:39:40 -0700

    I like the arrows better then ellipsis

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import { Card, CardText, Button } from 'react-md';
 import Link from "gatsby-link";
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import range from 'lodash/range';
 import clamp from 'lodash/clamp';
+import faAngleDoubleLeft from '@fortawesome/fontawesome-free-solid/faAngleDoubleLeft'
+import faAngleDoubleRight from '@fortawesome/fontawesome-free-solid/faAngleDoubleRight'
 
 function PaginationButton({page, index}) {
   const url = page === 1 ? "" : page.toString();
@@ -17,15 +20,15 @@ class Pagination extends Component {
   render() {
     const { index, pageCount } = this.props;
     const pages = range(clamp(index-5, 1, pageCount), clamp(index+10, pageCount)).slice(0, 10)
+    const angleDoubleLeft = <FontAwesomeIcon size="2x" style={{ margin: 'auto' }} fixedWidth  icon={faAngleDoubleLeft} />;
+    const angleDoubleRight = <FontAwesomeIcon size="2x" style={{ margin: 'auto' }} fixedWidth  icon={faAngleDoubleRight} />;
     // flex and center align it
     return (
       <Card raise className="md-grid md-cell md-cell--12">
-        <CardText>
-          <div className="md-grid">
-            {!pages.includes(1) && <span className="md-cell md-cell--1"><PaginationButton page={1} index={index} />...</span>}
-            {pages.map(page => <span key={page} className="md-cell md-cell--1"><PaginationButton page={page} index={index} /></span>)}
-            {!pages.includes(pageCount-1) && <span className="md-cell md-cell--1">...<PaginationButton page={pageCount-1} index={index} /></span>}
-          </div>
+        <CardText style={{ textAlign: 'center' }}>
+          {!pages.includes(1) && <Link to={"".toString()}><Button floating iconEl={angleDoubleLeft} /></Link>}
+          {pages.map(page => <PaginationButton key={page} page={page} index={index} />)}
+          {!pages.includes(pageCount-1) && <Link to={(pageCount-1).toString()} ><Button floating iconEl={angleDoubleRight} /></Link>}
         </CardText>
       </Card>
     );
