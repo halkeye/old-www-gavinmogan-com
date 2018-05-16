860b8245c12f619c25689fb5d8ab3345c404c189
Gavin Mogan <git@gavinmogan.com>
2018-05-15 21:51:00 -0700

    change blog to www

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -10,7 +10,7 @@ module.exports = {
   siteTitle: "The Nameless Site", // Site title.
   siteTitleAlt: "The Nameless Site", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://blog.gavinmogan.com", // Domain of your website without pathPrefix.
+  siteUrl: "https://www.gavinmogan.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "Meow Meow Meow Meow Neow?", // Website description used for RSS feeds/meta description tag.
