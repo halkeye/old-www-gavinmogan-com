26ac1430023850b0d212cdfc2440cb106c64fdda
Gavin Mogan <git@gavinmogan.com>
2018-09-12 00:07:40 -0700

    pull out profile image so its a little easier to test

---
+++ src/components/ProfileImage/ProfileImage.jsx
@@ -0,0 +1,30 @@
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+import Img from 'gatsby-image';
+
+const ProfileImage = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          file(relativePath: { eq: "Gavin-December-1989.png" }) {
+            childImageSharp {
+              fixed(width: 150, height: 150) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <Img
+          {...data.file.childImageSharp}
+          alt="Gavin December 1989"
+          className="profile-img"
+        />
+      )}
+    />
+  );
+};
+
+export default ProfileImage;
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,11 +1,30 @@
 import { graphql, StaticQuery, Link } from 'gatsby';
 import React from 'react';
 import Helmet from 'react-helmet';
-import Img from 'gatsby-image';
 import Navigation from '../components/Navigation/Navigation.jsx';
+import ProfileImage from '../components/ProfileImage/ProfileImage.jsx';
 import config from '../../data/SiteConfig.js';
 import './index.scss';
 import './global.scss';
+
+const Headers = () => {
+  return <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Helmet titleTemplate={`%s | ${data.site.siteMetadata.title}`} defaultTitle={data.site.siteMetadata.title}>
+        <meta name="description" content={config.siteDescription} />
+      </Helmet>
+    )}
+  />;
+};
 
 export default class MainLayout extends React.Component {
   render () {
@@ -13,57 +32,32 @@ export default class MainLayout extends React.Component {
       children, title
     } = this.props;
     return (
-      <StaticQuery
-        query={graphql`
-          query LayoutQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
-            file(relativePath: { eq: "Gavin-December-1989.png" }) {
-              childImageSharp {
-                fixed(width: 150, height: 150) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-          }
-        `}
-        render={data => (
-          <Navigation config={config} LocalTitle={title || 'Home'}>
-            <Helmet titleTemplate={`%s | ${data.site.siteMetadata.title}`} defaultTitle={data.site.siteMetadata.title}>
-              <meta name="description" content={config.siteDescription} />
-            </Helmet>
-            <div>
-              <div id="page">
-                <section id="body">{children}</section>
-                <section id="profile">
-                  <Img
-                    {...data.file.childImageSharp}
-                    alt="Gavin December 1989"
-                    className="profile-img"
-                  />
-                  <h1>Gavin Mogan</h1>
-                  <h2>
-                  Hi. I&apos;m Gavin. I&apos;m a coder at Sauce Labs. At home I
-                  code, game, hang out, all the cool non robot things to do.
-                  #notarobot
-                  </h2>
-                  <p>Friends Sites</p>
-                  <ul>
-                    <li>
-                      <a rel="friend" href="https://forgreatjustice.ca/">
-                      For Great Justice
-                      </a>{' '}
-                    (<Link to="/tags/nigel">Nigel</Link>)
-                    </li>
-                  </ul>
-                </section>
-              </div>
-            </div>
-          </Navigation>
-        )} />
+      <Navigation config={config} LocalTitle={title || 'Home'}>
+        <Headers />
+        <div>
+          <div id="page">
+            <section id="body">{children}</section>
+            <section id="profile">
+              <ProfileImage />
+              <h1>Gavin Mogan</h1>
+              <h2>
+                Hi. I&apos;m Gavin. I&apos;m a coder at Sauce Labs. At home I
+                code, game, hang out, all the cool non robot things to do.
+                #notarobot
+              </h2>
+              <p>Friends Sites</p>
+              <ul>
+                <li>
+                  <a rel="friend" href="https://forgreatjustice.ca/">
+                    For Great Justice
+                  </a>{' '}
+                  (<Link to="/tags/nigel">Nigel</Link>)
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </Navigation>
     );
   }
 }
