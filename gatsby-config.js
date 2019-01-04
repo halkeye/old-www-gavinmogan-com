e0a5f2b187295bf5933cc55ac21ac7254c012c7c
Gavin Mogan <git@gavinmogan.com>
2019-01-03 22:57:49 -0800

    eslint fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 const config = require('./data/SiteConfig');
-const url = require('url');
+const { URL } = require('url');
 
 const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
 
@@ -168,7 +168,7 @@ module.exports = {
       options: {
         setup (ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata;
-          ret.image_url = url.resolve(
+          ret.image_url = URL(
             config.siteUrl,
             'img/Gavin-December-1989-a2ce6e58e297f8bdabe2dcbf01e49e3d-0e94d.png'
           );
--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -52,32 +52,36 @@ class Disqus extends Component {
       config.pathPrefix +
       trimStart(postNode.fields.slug, '/');
     return (
-      <><Card className={classes.root}>
-        <CardHeader
-          title="Comments"
-          avatar={<Avatar><FontAwesomeIcon icon={fasComment} /></Avatar>}
-        />
-        <CardContent>
-          <ReactDisqusComments
-            shortname={config.disqusShortname}
-            identifier={post.title}
-            title={post.title}
-            url={url}
-            category_id={post.category_id}
-            onNewComment={this.notifyAboutComment}
+      <>
+        <Card className={classes.root}>
+          <CardHeader
+            title="Comments"
+            avatar={<Avatar><FontAwesomeIcon icon={fasComment} /></Avatar>}
           />
-        </CardContent>
-      </Card>'       '<Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left'
-        }}
-        autoHideDuration={6000}
-        onClose={this.onSnackbarDismiss}
-        open={this.state.toasts.length > 0}
-        ContentProps={{ 'aria-describedby': 'message-id' }}
-        message={this.state.toasts.map(m => (<span key={m.text} id="message-id">{m.text}</span>))}
-      /></>
+          <CardContent>
+            <ReactDisqusComments
+              shortname={config.disqusShortname}
+              identifier={post.title}
+              title={post.title}
+              url={url}
+              category_id={post.category_id}
+              onNewComment={this.notifyAboutComment}
+            />
+          </CardContent>
+        </Card>
+        {'       '}
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          autoHideDuration={6000}
+          onClose={this.onSnackbarDismiss}
+          open={this.state.toasts.length > 0}
+          ContentProps={{ 'aria-describedby': 'message-id' }}
+          message={this.state.toasts.map(m => (<span key={m.text} id="message-id">{m.text}</span>))}
+        />
+      </>
     );
   }
 }
