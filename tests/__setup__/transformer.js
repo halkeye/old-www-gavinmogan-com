f2dccc153f8964d80fed3c8546cc46b404eba625
Gavin Mogan <git@gavinmogan.com>
2019-10-06 11:57:26 -0700

    update tests a bit mre based on https://www.gatsbyjs.org/docs/unit-testing/

--- tests/__setup__/transformer.js
+++ tests/__setup__/transformer.js
@@ -1,6 +1,17 @@
 // test/transformer.js
 module.exports = require('babel-jest').createTransformer({
-  presets: ['@babel/react', '@babel/env'],
+  presets: [
+    '@babel/react',
+    '@babel/env',
+    [
+      'babel-preset-gatsby',
+      {
+        targets: {
+          browsers: ['>0.25%', 'not dead']
+        }
+      }
+    ]
+  ],
   plugins: [
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-class-properties',
