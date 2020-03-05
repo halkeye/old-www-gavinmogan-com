52af029f40d2a4eb8817242140b3f21568600337
Gavin Mogan <git@gavinmogan.com>
2020-03-04 19:06:55 -0800

    since moving to beta helmet, no longer ignore the depreciated warnings

--- tests/__setup__/frameworkSetup.js
+++ tests/__setup__/frameworkSetup.js
@@ -3,17 +3,11 @@ beforeEach(() => {
   isConsoleWarningOrError = false;
   const originalError = global.console.error;
   jest.spyOn(global.console, 'error').mockImplementation((...args) => {
-    if (args.join('').includes('Please update the following components: SideEffect(NullComponent)')) {
-      return;
-    }
     isConsoleWarningOrError = true;
     originalError(...args);
   });
   const originalWarn = global.console.warn;
   jest.spyOn(global.console, 'warn').mockImplementation((...args) => {
-    if (args.join('').includes('Please update the following components: SideEffect(NullComponent)')) {
-      return;
-    }
     isConsoleWarningOrError = true;
     originalWarn(...args);
   });
